91d9261345d0655d16198ee989450693bdf5f669
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-07 16:27:52 -0500

    resolvers and typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    password: String
+    email: String
+    bookcount: INT
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID
+    user: User
+  }
+`;
